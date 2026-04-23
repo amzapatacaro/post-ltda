876ff23f8511a8fb2fdbfd8df639e3ad1a36a0ae
Mauro Zapata <mauricio.zapata@caprelease.com>
2026-04-22 19:13:25 -0500

    fix(di): register JujuTestContext only via AddDbContext; resolve BaseModel from same scope

--- API/Startup.cs
+++ API/Startup.cs
@@ -23,21 +23,23 @@ namespace ProjectAPI.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //Servicios                      
-            services.AddScoped<JujuTestContext, JujuTestContext>();
-            services.AddScoped<BaseService<Customer>, BaseService<Customer>>();
-            services.AddScoped<BaseModel<Customer>, BaseModel<Customer>>();
-            services.AddScoped<BaseService<Post>, BaseService<Post>>();            
-            services.AddScoped<BaseModel<Post>, BaseModel<Post>>();
-
-
-            //Agregar cadena de conexion al contexto
             services.AddDbContext<JujuTestContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("Development")));
+                options.UseSqlServer(Configuration.GetConnectionString("Development"))
+            );
 
+            services.AddScoped(sp => new BaseModel<Customer>(
+                sp.GetRequiredService<JujuTestContext>()
+            ));
+            services.AddScoped(sp => new BaseModel<Post>(sp.GetRequiredService<JujuTestContext>()));
+
+            services.AddScoped(sp => new BaseService<Customer>(
+                sp.GetRequiredService<BaseModel<Customer>>()
+            ));
+            services.AddScoped(sp => new BaseService<Post>(
+                sp.GetRequiredService<BaseModel<Post>>()
+            ));
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
 
             // ======== CONFIGURACIÓN DE SWAGGER =========
             services.AddSwaggerGen(c =>
@@ -47,7 +49,6 @@ namespace ProjectAPI.API
             services.AddHttpContextAccessor();
             services.AddSession();
         }
-
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -65,13 +66,9 @@ namespace ProjectAPI.API
                 c.SwaggerEndpoint($"{swaggerJsonBasePath}/swagger/v1/swagger.json", "TestAPI v1");
             });
 
-            app.UseCors(options => options
-                .AllowAnyOrigin()
-                .AllowAnyMethod()
-                .AllowAnyHeader());
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
             app.UseAuthentication();
-
 
             app.UseSession();
             app.UseMvc();
